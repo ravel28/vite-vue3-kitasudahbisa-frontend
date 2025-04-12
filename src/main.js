378eda12bd06897c74d899944bb1124d14fba16b
ravel28 <ravelinno9@gmail.com>
2025-04-12 23:51:12 +0700

    [update] console.log to main.js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,15 @@ import './style.css'
 createApp(App)
   .use(router)
   .mount('#app');
+
+  console.log(
+    `%c Ravel28`,
+    'color: white; font-weight: bold; font-size: 200px;',
+  )
+  
+  
+  console.log(
+    `%c 🔥 Diam dan buktiikan`,
+    'color: #42b883; font-size: 14px; font-weight: bold;'
+  )
+  
