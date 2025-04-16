ed2652979910483f033ab692d29637651b1a938d
ravel28 <ravelinno9@gmail.com>
2025-04-16 23:10:38 +0700

    [update] integrasi login to backend

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,24 +1,38 @@
-<style>
-@media (max-width: 999px) {
-    .container {
-        width: 80%;
+<script setup>
+    import { ref } from 'vue';
+    import axios from 'axios';
+
+    const formLogin = ref({
+        user: '',
+        password: ''
+    })
+
+    const emit = defineEmits(['submit'])
+
+    const login = async () => {
+        console.table(formLogin.value);
+        try {
+            const response = await axios.post(import.meta.env.VITE_API_BASE_URL + '/users/login', formLogin.value)
+        } catch (error) {
+            console.error('Login failed:', error)
+        }
+        emit('submit', { ...formLogin.value })
     }
-}
-</style>
+</script>
 
 <template>  
     <div class="body w-screen flex justify-center items-center h-screen bg-linear-to-r/hsl from-indigo-500 to-teal-400 m-0">
         <div class="container grid grid-cols-1 lg:grid-cols-2 gap-2 bg-white rounded-lg w-6xl shadow-xl">
-            <div class="form-login p-15">
-                <form class="space-y-6 lg:w-lg w-full" action="#">
+                    <div class="form-login p-15">
+                <form class="space-y-6 lg:w-lg w-full" action="#" @submit.prevent="login">
                     <h5 class="text-xl font-medium text-gray-900">Sign in to absensi</h5>
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                        <input type="email" name="email" id="email" class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@gmail.com" required />
+                        <input type="email" name="email" id="email" class="border border-gray-300 text-sm rounded-lg block w-full p-2.5" placeholder="name@gmail.com" required v-model="formLogin.user"/>
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="border border-gray-300 text-sm rounded-lg block w-full p-2.5" required v-model="formLogin.password" />
                     </div>
                     <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3">Login</button>
                     <div class="text-sm font-medium text-gray-500 dark:text-gray-500">
@@ -28,10 +42,17 @@
             </div>
             <div class="catalog p-6 hidden lg:flex items-center justify-center">
                 <div class="catalog-info">
-                    <h1 class="mb-4 font-extrabold tracking-tight text-gray-500 md:text-3xl lg:text-3xl text-center">Kita Sudah Bisa 🧑‍💻</h1>
-                    <!-- <p class="dark:text-white text-sm">Website ini diciptakan hanya utnuk karyawan perusahaan untuk melakukan absensi agar laporan dapat dengan mudah di data oleh kami</p> -->
+                    <h1 class="mb-4 font-extrabold tracking-tight text-gray-500 md:text-3xl lg:text-3xl text-center">Kita  Sudah Bisa 🧑‍💻</h1>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+@media (max-width: 999px) {
+    .container {
+        width: 80%;
+    }
+}
+</style>
